8b82c3ef40162ecdec7e0f61b0e5d787e7fca557
Ilya Rubnich <ilya.rubnich@datadoghq.com>
2025-04-28 15:52:11 -0400

    move aliases to functions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,2 @@
-alias ll "ls -lah --color=auto"
-alias rfish ". $HOME/.config/fish/config.fish"
-
-# Added by `rbenv init` on Sat Jan 25 11:08:28 AM EST 2025
-status --is-interactive; and rbenv init - --no-rehash fish | source
+set __fish_git_prompt_showdirtystate 'yes'
 
---
+++ .config/fish/functions/ll.fish
@@ -0,0 +1,4 @@
+function ll --wraps=ls --wraps='ls -lah --color=auto' --description 'alias ll ls -lah --color=auto'
+  ls -lah --color=auto $argv
+        
+end
